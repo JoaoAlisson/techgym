f107512797b2ef16b91bcc29c1c092b3e458cf7e
João Alisson <j_alisson_bass@hotmail.com>
2014-04-22 10:39:49 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     //     return false;
     //});
 	//$("#nome1").tooltip();
+	$("#inicial").hide();
 	$("#inicial").fadeIn();
 	  setTimeout(function(){
 		 	//alert("teste");
